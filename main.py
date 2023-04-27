b4afb63b7a213d18a87a9df64418041704602926
jasonvandeyar <jasonvandeyar55@gmail.com>
2023-04-27 08:09:54 +0200

    create loop to send all SMS's based on close stock price difference between yesterday and today's stock price

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from twilio.rest import Client
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
+# STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-stock_api = "9R6AKE84BU9XSF46"
+stock_api = os.environ['STOCK_API']
 stock_url = "https://www.alphavantage.co/query?"
 
 stock_params = {
@@ -25,52 +25,59 @@ for stock in stock_data:
 
 current_day = float(stock_price[0])
 previous_day = float(stock_price[1])
-diff = current_day - previous_day
-five_percent = round((current_day/100) * 5, 2)
+diff = previous_day - current_day
+five_percent_pos = round((current_day/100) * 3, 2)
+five_percent_neg = round((current_day/100) * -3, 2)
+diff_percent = round(diff/(current_day/100), 2)
 
-print(diff)
-
-## STEP 2: Use https://newsapi.org
+# STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-# news_api = "6f93244a3f664550b2b409c0caf78a3a"
-# news_url = "https://newsapi.org/v2/everything"
-#
-# news_params = {
-#     "q": COMPANY_NAME,
-#     "from": "2023-03-23",
-#     "sortBy": "publishedAt",
-#     "apiKey": news_api
-# }
-#
-# news_res = requests.get(news_url, news_params)
-# news_data = news_res.json()["articles"][0:3]
+news_api = os.environ["NEWS_API"]
+news_url = "https://newsapi.org/v2/everything"
 
+news_params = {
+    "q": COMPANY_NAME,
+    "from": "2023-03-27",
+    "sortBy": "publishedAt",
+    "apiKey": news_api
+}
 
+news_res = requests.get(news_url, news_params)
+news_data = news_res.json()["articles"][0:3]
 
-## STEP 3: Use https://www.twilio.com
+# STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
-# account_sid = os.environ['TWILIO_ACCOUNT_SID']
-# auth_token = os.environ['TWILIO_AUTH_TOKEN']
-# from_number = os.environ['TWILIO_FROM_NUMBER']
-# to_number = os.environ['TWILIO_TO_NUMBER']
-#
-# client = Client(account_sid, auth_token)
-#
-# message = client.messages.create(
-#                               from_=from_number,
-#                               body='Twilio message test.',
-#                               to=to_number
-#                           )
+account_sid = os.environ["TWILIO_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+from_number = os.environ["FROM_NUMBER"]
+to_number = os.environ["TO_NUMBER"]
 
+client = Client(account_sid, auth_token)
+
+if diff >= five_percent_pos or diff <= five_percent_neg:
+
+    for article in news_data:
+        description = article["description"]
+        title = article["title"]
+        sms_message = f"{STOCK}: {diff_percent}\n Headline: {title}\n Brief: {description}"
+        message = client.messages.create(
+                                      from_=from_number,
+                                      body=sms_message,
+                                      to=to_number
+                                  )
 # print(message.sid)
 
 # Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file
+ by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
+ coronavirus market crash.
 or
 "TSLA: 🔻5%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file
+ by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the
+  coronavirus market crash.
 """
